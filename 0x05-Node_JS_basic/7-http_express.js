a6cb32a92876decdfaa82a916866ee983dae498a
CyrusNchege <cyruschegecc@gmail.com>
2023-11-21 19:24:54 +0000

    Refactor HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { argv } = require('process');
-const fs = require('fs').promises;
+const fs = require('fs');
 
 const app = express();
 
@@ -9,39 +9,29 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
+app.get('/students', (req, res) => {
   res.set('Content-Type', 'text/plain');
   res.write('This is the list of our students\n');
-
-  try {
-    const data = await fs.readFile(argv[2], 'utf8');
-    const result = data
-      .split('\n')
-      .map((line) => line.split(','))
-      .filter((check) => check.length > 3);
-
-    const newis = result.map((data) => [data[0], data[3]]);
-    const fields = new Set(newis.map((item) => item[1]));
-    const final = {};
-
-    fields.forEach((data) => {
-      final[data] = 0;
-    });
-
-    newis.forEach((data) => {
-      final[data[1]] += 1;
-    });
-
-    res.write(`Number of students: ${result.length}\n`);
-    
-    for (const field in final) {
-      res.write(`Number of students in ${field}: ${final[field]}. List: ${newis.filter((n) => n[1] === field).map((n) => n[0]).join(', ')}\n`);
+  fs.readFile(argv[2], 'utf8', (err, data) => {
+    if (err) {
+      throw Error('Cannot load the database');
     }
-
+    const result = [];
+    data.split('\n').forEach((data) => {
+      result.push(data.split(','));
+    });
+    result.shift();
+    const newis = [];
+    result.forEach((data) => newis.push([data[0], data[3]]));
+    const fields = new Set();
+    newis.forEach((item) => fields.add(item[1]));
+    const final = {};
+    fields.forEach((data) => { (final[data] = 0); });
+    newis.forEach((data) => { (final[data[1]] += 1); });
+    res.write(`Number of students: ${result.filter((check) => check.length > 3).length}\n`);
+    Object.keys(final).forEach((data) => res.write(`Number of students in ${data}: ${final[data]}. List: ${newis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}\n`));
     res.end();
-  } catch (error) {
-    res.status(500).send('Internal Server Error\n');
-  }
+  });
 });
 
 app.listen(1245);
