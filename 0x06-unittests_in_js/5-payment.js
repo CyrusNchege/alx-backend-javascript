45fefefe53b8cf0dd4fdc9f13f567d6c0ed08b53
CyrusNchege <cyruschegecc@gmail.com>
2023-11-23 20:06:15 +0000

    Fix hooks

--- 0x06-unittests_in_js/5-payment.js
+++ 0x06-unittests_in_js/5-payment.js
@@ -1,8 +1,7 @@
 const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${sum}`);
+  console.log(`The total is: ${Utils.calculateNumber('SUM', totalAmount, totalShipping)}`);
 }
 
 module.exports = sendPaymentRequestToApi;
