1f19cfa27a27fce68ae283ad6328e0367a4482b8
CyrusNchege <cyruschegecc@gmail.com>
2023-11-24 20:05:24 +0000

    Refactor Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,34 +1,19 @@
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api');
 
-describe('Index page', function () {
-  // Test the correct status code
-  it('Correct status code?', function (done) {
-    request('http://localhost:7865', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      done();
+describe('Integration Testing', () => {
+  describe('GET /', () => {
+    it('Code: 200 | Body: Welcome to the payment system', (done) => {
+      const options = {
+        url: 'http://localhost:7865',
+        method: 'GET',
+      };
+
+      request(options, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
     });
-  });
-
-  // Test the correct result
-  it('Correct result?', function (done) {
-    request('http://localhost:7865', (error, response, body) => {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-
-  // Additional test (e.g., test the presence of certain headers)
-  it('Other?', function (done) {
-    request('http://localhost:7865', (error, response, body) => {
-      // Add your assertions here as needed
-      done();
-    });
-  });
-
-  // Test the server
-  it('Server', function () {
-    expect(app).to.not.be.undefined;
   });
 });
